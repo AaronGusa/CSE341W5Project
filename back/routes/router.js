d139b58450e9bcdc7e3d3f4b975fc6ceb79c2471
Goosa2025 <gus21002@byui.edu>
2023-02-04 22:59:19 -0700

    Swagger integration

--- back/routes/router.js
+++ back/routes/router.js
@@ -1,10 +1,25 @@
 const r = require('express').Router();
 
-r.get('/', (req, res, next) => {
-    res.send("We are routered!");
-})
+// r.get('/', (req, res, next) => {
+//     res.send("We are routered!");
+// })
+
+r.use('/', require('./swagger'))
 
 r.use('/bus', require('./business'))
+
+r.use('/', 
+  // require('../../front/new.html')
+  (docData = (req, res) => {
+    
+  let docData = {
+    APIs: 'https://agw05cse341.onrender.com/api-docs/',
+    Contact_JSON: 'https://agw05cse341.onrender.com/contacts',
+  };
+  res.send(docData);
+})
+  
+)
 
 
 module.exports = r;
